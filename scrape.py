64b2493850791fc5f4af1d8995c26f30742eac34
itisalvinn <alvintfng@gmail.com>
2021-02-24 00:50:25 -0500

    add scraper and helper tools

---
+++ scrape.py
@@ -0,0 +1,41 @@
+import requests
+import re
+
+from bs4 import BeautifulSoup
+from tools import getColWidth, writeToExcel
+
+# TODO: add read me + note the data scraped is based on Finviz updates (i.e. reflects whatever is during stock market hours)
+# general setup 
+# we will target Finviz top gainers table (LHS of FINVIZ home page)
+url = "https://www.finviz.com/"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+table = soup.find('table', class_='t-home-table')
+
+data = []
+tblHeaders = ['Ticker', 'Change %', 'Industry', 'Company Name', 'Country', 'Market Cap']
+colWidths = getColWidth(tblHeaders)
+
+# scrape data in top gainers table
+for idx, stock in enumerate(table):
+
+    # data is available for even number elements > 0
+    if idx % 2 == 0 and idx != 0:
+        ticker = stock.find('a').get_text()
+        change = stock.find('span').get_text()
+        ticker_info = stock.find('td')
+        companySearch = re.search(r'(?<=\b&gt;)(.*?)(?=\&lt)', str(ticker_info))
+        company = companySearch.group(0)
+        hoverSearch = re.search(r'(?<=\;br&gt;\s)(.*?)(?=\])', str(ticker_info))
+        hover = hoverSearch.group(0).replace('&amp; ', '')
+        hover = hover.split('|')
+        data.append([ticker, change, company, hover[0], hover[1], hover[2]])
+
+        if len(company) > colWidths[3]:
+            colWidths[3] = len(company)
+        if len(hover[0]) > colWidths[2]:
+            colWidths[2] = len(hover[0])
+
+# write data to excel file
+writeToExcel(data, tblHeaders, colWidths)
---
+++ tools.py
@@ -0,0 +1,68 @@
+import xlsxwriter
+from datetime import date
+
+def getColWidth(tblHeaders):
+    """ 
+    method to get length of each column header
+    
+    :param tblHeaders: 6 columns - ticker, change, company, industry, country, marketCap
+    :return: colWidths array containing length of each default col header  
+    """
+
+    colWidths = []
+
+    # default 2 units wider than length of original header
+    for i in range(len(tblHeaders)):
+        colWidths.append(len(tblHeaders[i]) + 2)
+
+    return colWidths
+
+def writeToExcel(data, tblHeaders, colWidths) -> None:
+    """
+    method to write scraped data into an excel file 
+    
+    :param data: array of tuples containing stock data scraped from Finviz
+    :param tblHeaders: array of headers for excel table
+    :param colWidths: array of width lengths 
+    :return: none
+    """
+
+    today = str(date.today())
+    fileName = today + '.xlsx'
+    path = 'gains/' + fileName
+
+    workbook = xlsxwriter.Workbook(path)
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': 1})
+    red = workbook.add_format({'font_color': 'red'})
+
+    worksheet.write('A1', tblHeaders[0], bold)
+    worksheet.write('B1', tblHeaders[1], bold)
+    worksheet.write('C1', tblHeaders[2], bold)
+    worksheet.write('D1', tblHeaders[3], bold)
+    worksheet.write('E1', tblHeaders[4], bold)
+    worksheet.write('F1', tblHeaders[5], bold)
+
+    row = 1
+    col = 0
+    
+    for ticker, change, company, industry, country, marketCap in (data):
+        worksheet.write_string(row, col, ticker)
+        # negative change highlighted in red
+        if float(change.strip('%')) > 0:
+            worksheet.write_string(row, col+1, change)
+        else:
+            worksheet.write_string(row, col+1, change, red)
+        worksheet.write_string(row, col+2, industry)
+        worksheet.write_string(row, col+3, company)
+        worksheet.write_string(row, col+4, country)
+        worksheet.write_string(row, col+5, marketCap)
+
+        row += 1
+    
+    for i in range(len(colWidths)):
+        worksheet.set_column(i, i, colWidths[i])
+
+    workbook.close()
+
+    print("File created in " + path)
